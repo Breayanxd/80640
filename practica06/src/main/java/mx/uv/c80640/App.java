4be7580fd82ad51219762c9fde2e69ca13274109
Breayanxd <breayanmt10@hotmail.com>
2022-10-20 12:59:37 -0500

    20/10/22

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,4 +1,5 @@
 package mx.uv.c80640;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -9,5 +10,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/Juan", (req, res) -> "Hello Juan");
+        get("/Pepito", (req, res) -> "Hello Pepito");
+        get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
     }
 }
