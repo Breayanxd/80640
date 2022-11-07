b0965be35d5ad990f052e58efed8c4b01b4b59e7
Breayanxd <breayanmt10@hotmail.com>
2022-11-06 20:37:09 -0600

    javaScript

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,6 +1,7 @@
 package mx.uv.c80640;
 import static spark.Spark.*;
 
+import com.google.gson.JsonObject;
 /**
  * Hello world!
  *
@@ -13,5 +14,16 @@ public class App
         get("/Juan", (req, res) -> "Hello Juan");
         get("/Pepito", (req, res) -> "Hello Pepito");
         get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
+        get("/", (req, res) -> "hola");
+        
+        post("/", (req, res) ->{
+            System.out.println(req.queryParams("email")+" "+ req.queryParams("password"));
+            System.out.println(req.body());
+            res.status(200);
+            JsonObject oRespuesta= new JsonObject();
+            oRespuesta.addProperty("msj","hola");
+            oRespuesta.addProperty("email", req.queryParams("email"));
+            return oRespuesta;
+        });
     }
 }
